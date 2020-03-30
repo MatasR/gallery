b75f78a53bc44ea432b7beee987e793d1f51cb63
MatasR <batassp@gmail.com>
2020-03-30 11:35:14 +0300

    Modal done for now

--- resources/views/includes/popup.blade.php
+++ resources/views/includes/popup.blade.php
@@ -3,14 +3,14 @@
     <div class="modal-content">
 
       <div class="modal-header">
-        <img src="/storage/images\March2020\QXUIFsyZwNeeQTPQeC0X.jpg">
+        <img src>
         <button type="button" class="close bg-white rounded-circle" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
 
       <div class="modal-body">
-        <h2 class="display-4 mb-3 d-inline-block">Varliukai</h2>
+        <h2 class="display-4 mb-3 d-inline-block"></h2>
         <p class="mb-0">
           Change the alignment of elements with the vertical-alignment utilities. Please note that vertical-align only affects inline, inline-block, inline-table, and table cell elements.
         </p>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -54,11 +54,11 @@
           @foreach($category->products->merge($category->childs_products) as $product)
             <div {{ ($category->slug == $initCategory->slug ? 'data-aos=fade-up' : '') }} id="{{ $product->id }}" class="card text-white text-center border-0 {{ ($product->category->parent?$product->category->parent->slug:'') }} {{ $product->category->slug }}">
               <img class="card-img" src="{{ asset('storage/'.$product->image->src) }}"/>
-              <div class="card-img-overlay">
+              <!--<div class="card-img-overlay">
                 <h4 class="card-title font-weight-normal position-absolute fixed-bottom mb-0 py-2">
                   {{ $product->title }}
                 </h4>
-              </div>
+              </div>-->
             </div>
           @endforeach
         </div>
