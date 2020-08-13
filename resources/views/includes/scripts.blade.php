5b2316f9bf34a48db3510eccff90feabce019b4d
MatasR <batassp@gmail.com>
2020-08-13 16:39:38 +0300

    analytics

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -5,3 +5,13 @@
   AOS.init();
 </script>
 @stack('scripts')
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-175306336-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-175306336-1');
+</script>
