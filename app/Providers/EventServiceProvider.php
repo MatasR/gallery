060c28c35f8c199a32a8e49d7bb8f92e4d861356
MatasR <batassp@gmail.com>
2020-10-03 22:49:38 +0300

    Author fullname auto generate on data change

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -15,8 +15,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        \TCG\Voyager\Events\BreadDataChanged::class => [
+            \App\Listeners\MakeAuthorFullname::class,
         ],
     ];
 
